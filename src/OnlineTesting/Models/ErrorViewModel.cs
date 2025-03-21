aebbbce97b94264e50c8b7629e0e5a3e67bc9f84
Stanislav_Hromak <gromakstanislav@knu.ua>
2025-03-21 20:00:46 +0200

    Initial commit

---
+++ src/OnlineTesting/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace OnlineTesting.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
